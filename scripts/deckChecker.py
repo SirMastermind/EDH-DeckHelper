2a4453e726e762f3463fba09fc3051729f0ba85d
Tomás Alves <sir.mastermind94@gmail.com>
2019-09-22 10:47:46 +0100

    No more warnings

--- scripts/deckChecker.py
+++ scripts/deckChecker.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+# import warnings
+# warnings.filterwarnings("ignore")
 
 if len(sys.argv) < 2:
     print("No argument")
@@ -90,7 +92,7 @@ else:
 
                     diff_df = deck_df[(~deck_df['Name'].isin(merged_inner['Name']))]
                     
-                    diff_df.sort_values(by=['Name'], inplace=True)
+                    diff_df = diff_df.sort_values(by=['Name'])
 
                     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
                         print(diff_df)
@@ -126,17 +128,17 @@ else:
         	if file.endswith(".csv") and "._" not in file:
         		fname = "./data/binder/" + file
         binder_df = pd.read_csv(fname, delimiter=';')
-        binder_df.dropna(inplace=True, subset=['Name'])
-        binder_df['Have'].fillna("No", inplace=True)
+        binder_df = binder_df.dropna(subset=['Name'])
+        binder_df['Have'] = binder_df['Have'].fillna("No")
         binder_df['Have'] = binder_df['Have'].astype(str)
-        binder_df['Have'].replace(to_replace='1.0', value='Yes', inplace=True)
+        binder_df['Have'] = binder_df['Have'].replace(to_replace='1.0', value='Yes')
         dontHave_df = binder_df.loc[binder_df['Have'] == "No"]
-        dontHave_df.drop(['Slot', 'Have'], inplace=True, axis=1)
+        dontHave_df = dontHave_df.drop(['Slot', 'Have'], axis=1)
         output_df = pd.concat([output_df, dontHave_df])
         print("Taking out duplicates...")
-        output_df.drop_duplicates(keep='first', inplace=True)
+        output_df = output_df.drop_duplicates(keep='first')
         print("Sorting cards by name...")
-        output_df.sort_values(by=['Name'], inplace=True)
+        output_df = output_df.sort_values(by=['Name'])
         for value in output_df['Name']:
             output.write(value + '\n')
         print("File is ready!")
