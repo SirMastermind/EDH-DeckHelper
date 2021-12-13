c0f0734803c899e65b7257d7ec4e7b72dbd80fde
Tomás Alves <sir.mastermind94@gmail.com>
2021-12-13 17:37:34 +0000

    Function to check Common Cards between decks now allows for any number of decks

--- scripts/checkCommonCards.py
+++ scripts/checkCommonCards.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import sys
 import os
-print("Choose a deck to continue:")
+print("Choose a list of decks to continue:")
 i = 1
 for file in os.listdir("./data/decks"):
     if file.endswith(".csv"):
@@ -15,23 +15,17 @@ for file in os.listdir("./data/decks"):
         print("%d - %s" % (i, file.split(".csv")[0]))
         i += 1
 
-deck_1 = input("Choose the first deck by number: ")
+decks = input("Enter the decks by their number separated by a '-': ")
 
-i = 1
-for file in os.listdir("./data/decks"):
-    if file.endswith(".csv"):
-        fname = "./data/decks/" + file
+list_decks = decks.split('-')
 
-        if "._" in fname:
-            continue
+for deck in list_decks:
+    if deck == '':
+        print("Incorrect input form...")
+        exit()
 
-        print("%d - %s" % (i, file.split(".csv")[0]))
-        i += 1
-
-deck_2 = input("Choose the second deck by number: ")
-
-if int(deck_1) < 1 or int(deck_2) < 1:
-    print("At least one of those decks doesn't exist.")
+if len(list_decks) < 2:
+    print("That is less than two decks...")
     exit()
 
 i = 1
@@ -40,9 +34,9 @@ for file in os.listdir("./data/decks"):
         fname = "./data/decks/" + file
         if "._" in fname:
             continue
-        if (int(deck_1) == i):
-            deck_1_df = pd.read_csv(fname, delimiter=',', header=None)
-            deck_1_df.columns = ['Number', 'Name']
+        if (int(list_decks[0]) == i):
+            output_df = pd.read_csv(fname, delimiter=',', header=None)
+            output_df.columns = ['Number', 'Name']
         i += 1
 
 i = 1
@@ -51,14 +45,31 @@ for file in os.listdir("./data/decks"):
         fname = "./data/decks/" + file
         if "._" in fname:
             continue
-        if (int(deck_2) == i):
+        if (int(list_decks[1]) == i):
             deck_2_df = pd.read_csv(fname, delimiter=',', header=None)
             deck_2_df.columns = ['Number', 'Name']
         i += 1
 
+merged_inner = pd.merge(left=output_df, right=deck_2_df, left_on='Name', right_on='Name')
 
-merged_inner = pd.merge(left=deck_1_df, right=deck_2_df, left_on='Name', right_on='Name')
-
+if len(list_decks) > 2:
+    deck = 2
+    while deck < len(list_decks):
+        if list_decks[deck] == '':
+            break
+        i = 1
+        for file in os.listdir("./data/decks"):
+            if file.endswith(".csv"):
+                fname = "./data/decks/" + file
+                if "._" in fname:
+                    continue
+                if (int(list_decks[deck]) == i):
+                    deck_df = pd.read_csv(fname, delimiter=',', header=None)
+                    deck_df.columns = ['Number', 'Name']
+                    merged_inner = pd.merge(left=merged_inner, right=deck_df, left_on='Name', right_on='Name')
+                i += 1
+        deck+=1
+        
 with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
     print(merged_inner[['Name']])
 
